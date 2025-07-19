ae7aac56c4fc107f5577432ac5293d07c3694eba
Kauan Vidigal <kauanvidigalcontato@gmail.com>
2025-07-18 22:52:21 -0300

    creating database module
    Separação do database module DDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,7 @@
-import {Module} from '@nestjs/common';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {ConfigModule} from '@nestjs/config';
-import {Favorites} from "@/domain/favorites/favorite.entity";
-import {Users} from "@/domain/users/user.entity";
-import {Medias} from "@/domain/medias/media.entity";
-import {Langs} from "@/domain/langs/lang.entity";
-import {Genres} from "@/domain/genres/genre.entity";
+import { Module } from '@nestjs/common';
+import {DatabaseModule} from "@/shared/database/database.module";
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: '.env',
-        }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: process.env.POSTGRES_HOST ?? 'localhost',
-            port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
-            username: process.env.POSTGRES_USER ?? 'vidigal',
-            password: process.env.POSTGRES_PASSWORD ?? 'test1234',
-            database: process.env.POSTGRES_DB ?? 'media_db',
-            entities: [Favorites, Genres, Langs, Users, Medias],
-            synchronize: true,
-        }),
-    ],
+    imports: [DatabaseModule],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ src/shared/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Favorites } from '@/domain/favorites/favorite.entity';
+import { Users } from '@/domain/users/user.entity';
+import { Medias } from '@/domain/medias/media.entity';
+import { Langs } from '@/domain/langs/lang.entity';
+import { Genres } from '@/domain/genres/genre.entity';
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: '.env',
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.POSTGRES_HOST ?? 'localhost',
+            port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
+            username: process.env.POSTGRES_USER ?? 'vidigal',
+            password: process.env.POSTGRES_PASSWORD ?? 'test1234',
+            database: process.env.POSTGRES_DB ?? 'media_db',
+            entities: [Favorites, Genres, Langs, Users, Medias],
+            synchronize: true,
+        }),
+    ],
+})
+export class DatabaseModule {}
